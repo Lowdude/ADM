a9b01dd48bbce90b599e99d81aff5dabf37a10db
Jonathan Irmscher <jonirmscher@t-online.de>
2021-05-07 16:01:45 +0200

    Changed output formatting, it is now possible to specify the number of digits printed in the source code

--- MCSolver.hs
+++ MCSolver.hs
@@ -1,7 +1,6 @@
 module MCSolver (dtmcSolver, dtmcSolverVerbose, discretise) where
 import MCParser ( DTMC(..), CTMC(..) )
 import Numeric.LinearAlgebra as NL
-    ( (<#), rows, ident, Linear(scale), Vector )
 import qualified Data.Vector as V
 import Control.Monad.State.Lazy
     ( MonadIO(liftIO), StateT, MonadState(put, get), runState, State )
@@ -16,7 +15,7 @@ dtmcSolver :: Int -> StateT DTMC IO()
 dtmcSolver n
     | n == 0 = do
         dtmc <- get
-        liftIO $ print (dtmc_vector dtmc)
+        liftIO $ disp 4 $ asRow (dtmc_vector dtmc)
     | otherwise = do
         dtmc <- fmap (runState _iter) get
         put (snd dtmc)
@@ -30,7 +29,7 @@ dtmcSolverVerbose n
     | otherwise = do
         dtmc <- fmap (runState _iter) get
         put (snd dtmc)
-        liftIO $ print (dtmc_vector (snd dtmc))
+        liftIO $ disp 4 $ asRow (dtmc_vector (snd dtmc)) -- Specify printed precision here
         dtmcSolverVerbose (n-1)
 
 discretise :: Double -> CTMC -> DTMC
